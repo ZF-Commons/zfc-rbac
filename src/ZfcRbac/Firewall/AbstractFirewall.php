f9a10266cc787bd45406b1fefa4da4c594dacc66
arekkas <aeneas.rekkas@serlo.org>
2013-11-14 16:39:54 +0100

    Removed 'use'

--- src/ZfcRbac/Firewall/AbstractFirewall.php
+++ src/ZfcRbac/Firewall/AbstractFirewall.php
@@ -3,11 +3,11 @@
 namespace ZfcRbac\Firewall;
 
 use ZfcRbac\Service\Rbac;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 abstract class AbstractFirewall
 {
     
-    use \Zend\ServiceManager\ServiceLocatorAwareTrait;
     
     const SEPARATOR = ':';
 
@@ -15,6 +15,34 @@ abstract class AbstractFirewall
      * @var Rbac
      */
     protected $rbac;
+
+    /**
+     * @var ServiceLocatorInterface
+     */
+    protected $serviceLocator = null;
+
+    /**
+     * Set service locator
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return mixed
+     */
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator = NULL)
+    {
+        $this->serviceLocator = $serviceLocator;
+
+        return $this;
+    }
+
+    /**
+     * Get service locator
+     *
+     * @return ServiceLocatorInterface
+     */
+    public function getServiceLocator()
+    {
+        return $this->serviceLocator;
+    }
 
     /**
      * @param  Rbac $rbac
