cd140f0b0ae00c11175f334ef6b06922b4d8b9d9
arekkas <aeneas.rekkas@serlo.org>
2014-02-18 16:44:35 +0100

    Added test ObjectRepositoryRoleProviderTest:testRoleCacheOnConsecutiveCalls()

--- tests/ZfcRbacTest/Role/ObjectRepositoryRoleProviderTest.php
+++ tests/ZfcRbacTest/Role/ObjectRepositoryRoleProviderTest.php
@@ -111,6 +111,19 @@ class ObjectRepositoryRoleProviderTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('memberguest', $childRolesString);
     }
 
+    public function testRoleCacheOnConsecutiveCalls()
+    {
+        $objectRepository = $this->getMock('Doctrine\ORM\EntityRepository', ['findBy'], [], '', false);
+        $memberRole       = new FlatRole('member');
+        $provider         = new ObjectRepositoryRoleProvider($objectRepository, 'name');
+        $result           = [$memberRole];
+
+        $objectRepository->expects($this->once())->method('findBy')->will($this->returnValue($result));
+
+        $this->assertEquals($result, $provider->getRoles(['member']));
+        $this->assertEquals($result, $provider->getRoles(['member']));
+    }
+
     public function testThrowExceptionIfAskedRoleIsNotFound()
     {
         $this->serviceManager = ServiceManagerFactory::getServiceManager();
