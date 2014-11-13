8e4778466e0bc74158e022a646719ad1f2edc61f
elhachmi <elhachmi@gmail.com>
2014-11-13 09:52:59 +0000

    Methode assert declaration raise a fatal error.
    Methode assert declaration raise a fatal error in php5.5 Fatal error strict standard $context param mus apprear in the function declaration

--- src/ZfcRbac/Assertion/AssertionInterface.php
+++ src/ZfcRbac/Assertion/AssertionInterface.php
@@ -39,5 +39,5 @@ interface AssertionInterface
      * @param  mixed                $context
      * @return bool
      */
-    public function assert(AuthorizationService $authorizationService, $context);
+    public function assert(AuthorizationService $authorizationService, $context );
 }
