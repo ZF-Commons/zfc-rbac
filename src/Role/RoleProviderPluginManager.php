03c81a39decddf2b8b81ff73e0753067510eb227
Michaël Gallego <mic.gallego@gmail.com>
2015-11-08 17:22:05 +0100

    Remove remove remove

--- src/Role/RoleProviderPluginManager.php
+++ src/Role/RoleProviderPluginManager.php
@@ -59,12 +59,4 @@ class RoleProviderPluginManager extends AbstractPluginManager
             is_object($plugin) ? get_class($plugin) : gettype($plugin)
         ));
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function canonicalizeName($name)
-    {
-        return $name;
-    }
 }
